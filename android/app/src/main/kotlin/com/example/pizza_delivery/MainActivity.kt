f2cc8c0ce9db25642705ccfc69320af6e3a2af60
pruthvi145 <pruthvipatel145@gmail.com>
2024-02-21 19:10:01 +0530

    fix: embedded version

--- android/app/src/main/kotlin/com/example/pizza_delivery/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pizza_delivery/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pizza_delivery
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
