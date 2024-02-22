eb597500996d8e680b6c25837e7f7ab7512cd62f
pruthvi145 <pruthvipatel145@gmail.com>
2024-02-22 18:49:38 +0530

    fix: kotlin issue

--- android/app/src/main/kotlin/com/example/pizza_delivery/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pizza_delivery/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.pizza_delivery
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+
   }
 }
